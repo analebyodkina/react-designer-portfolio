7321239ca15e0e1a50a45a3b5a04334f32f820be
analebyodkina <dp030992cad@gmail.com>
2023-04-27 10:58:41 +0300

    added projects section with swiper

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,9 +1,35 @@
 import "./ProjectsSection.scss";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import ProjectImage1 from "../../Assets/projects-img1.png";
+import ProjectImage2 from "../../Assets/projects-img2.png";
+import ProjectImage3 from "../../Assets/projects-img3.png";
 
+
+ 
 
 export default function ProjectsSection() {
     
     return (
-        <>Projects works</>
+      <div className="container projects">
+        <h2 className="title projects__title">Personal Projects</h2>
+        <Swiper 
+        spaceBetween={24}
+        slidesPerView={3}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide><img src={ProjectImage1} alt="project example" /></SwiperSlide>
+      <SwiperSlide><img src={ProjectImage2} alt="project example" /></SwiperSlide>
+      <SwiperSlide><img src={ProjectImage3} alt="project example" /></SwiperSlide>
+      <SwiperSlide><img src={ProjectImage2} alt="project example" /></SwiperSlide>
+      ...
+    </Swiper>
+
+      </div>
+      
+
+          
+        
     );
   }
