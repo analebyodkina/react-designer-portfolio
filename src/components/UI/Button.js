d9b584257ef8a047a59c3463d917ead7a2f86749
analebyodkina <dp030992cad@gmail.com>
2023-04-30 17:30:43 +0300

    made modal window more correct and no-scroll underneath

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,13 +1,23 @@
 import MailIcon from "../Icons/MailIcon";
+import Modal from 'react-modal';
+import { useState, useEffect } from 'react';
 import "./Button.scss";
 import React from 'react';
-import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
 
-function Button() {
+   function Button() {
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isModalOpen])
 
   function openModal() {
     setIsOpen(true);
@@ -18,17 +28,26 @@ function Button() {
   }
   return (
     <>
-    <button className="btn" onClick={openModal}>
+    <button className="btn" onClick={() => {openModal(); setIsModalOpen(true);}}> 
         <MailIcon /> 
         hi@yourname.com
         <div id="spin"></div>
     </button> 
-    <Modal 
-        isOpen={modalIsOpen}        
+    <Modal         
+        isOpen={isModalOpen} 
         onRequestClose={closeModal}        
         contentLabel="Example Modal"
-        className="Modal"  
-    >        
+        className="modal" 
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.75)',
+            zIndex: 9999,
+            overflow: "hidden",
+          },          
+        }} 
+
+        
+    >       
 
       <div className="modal__container">
         <div className="modal__body">
@@ -38,17 +57,13 @@ function Button() {
             <input className="modal__input" type="email" placeholder="Enter your email" />
             <button type="submit" className="modal__button">Submit</button>				
           </form>
-          <div className="modal__close" onClick={closeModal}>
+          <div className="modal__close" onClick={() => {closeModal(); setIsModalOpen(false);}}>
             <span className="material-symbols-outlined">close</span>
           </div>
         </div>
       </div>
-            
-          
         
-        
-        
-      </Modal>
+    </Modal>
       
     </>
     
